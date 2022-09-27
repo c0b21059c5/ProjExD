e7d445d602a79af261674f3c1f9364115906ad7c
retsu <c0b21059c5@edu.teu.ac.jp>
2022-09-27 17:14:28 +0900

    消えたアルファベット

---
+++ Ex01/kieta_alphabet.py
@@ -0,0 +1,44 @@
+from random import sample, randint, shuffle
+
+alphalist=list("QWERTYUIOPASDFGHJKLZXCVBNM")
+#num_taisyoumozi = randint(2, len(alphalist))
+#num_hyouzi = randint(1, (num_taisyoumozi-1))
+num_taisyoumozi=10
+num_hyouzi=8
+num_kessonmozi=num_taisyoumozi-num_hyouzi
+
+taisyoulist=[]
+kessonlist=[]
+hyouzilist=[]
+
+def shutudai():
+    #num_taisyoumozi = randint(2, len(alphalist))
+    #num_hyouzi = randint(1, (num_taisyoumozi-1))
+
+    taisyoulist = sample(alphalist, num_taisyoumozi)
+    hyouzilist = sample(taisyoulist, num_hyouzi)
+    kessonlist = list(set(taisyoulist)-set(hyouzilist))
+
+    print("対象")
+    print(taisyoulist)
+    print("表示")
+    print(hyouzilist)
+    print("欠損")
+    print(kessonlist)
+
+
+    a1=int(input("欠損文字はいくつあるでしょうか?:"))
+    if num_kessonmozi == a1:
+        print("正解です。それでは、具体的に欠損文字を１つずつ入力してください")
+        for i in range(num_kessonmozi):
+            a=input(str(i+1)+"つ目の文字を入力してください")
+            if a not in kessonlist:
+                print("不正解です")
+    else:
+        print("不正解です。またチャレンジしてください")
+
+    print("おめでとうございます！")
+    
+
+if __name__ == "__main__":
+    shutudai()
