be8cf80c99d7836710f0c454027cf407fa9a0a97
retsu <c0b21059c5@edu.teu.ac.jp>
2022-10-04 16:34:33 +0900

    改良

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -3,7 +3,7 @@ import tkinter.messagebox as tkm
 
 root=tk.Tk()
 root.title("電卓")
-root.geometry("300x600")
+root.geometry("400x700")
 
 def button_click(event):
     btn = event.widget
@@ -17,24 +17,47 @@ def click_equal(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, res)
 
+def click_clear(event):
+    entry.delete(0, tk.END)
+
 r=1
 c=0
+
+btn = tk.Button(root, text=f"AC", font=("Times New Roman", 30), width=4, height=2)
+btn.bind("<1>", click_clear)
+btn.grid(row=r, column=c)
+
+btn = tk.Button(root, text=".", font=("Times New Roman", 30), width=4, height=2)
+btn.bind("<1>", button_click)
+btn.grid(row=5, column=2)
+
 numbers = list(range(9, -1, -1))
-operators = ["+"]
-for i, num in enumerate(numbers+operators, 1):
+operators = ["/", "*", "-", "+"]
+for i, num in enumerate(numbers, 1):
     btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width=4, height=2)
     btn.bind("<1>", button_click)
-    btn.grid(row=r, column=c)
+    btn.grid(row=r+1, column=c)
     c += 1
     if i%3 == 0:
         r += 1
         c = 0
 
-entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
-entry.grid(row=0, column=0, columnspan=3)
+r=1
+c=0
+for i, num in enumerate(operators, 1):
+    btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width=4, height=2)
+    btn.bind("<1>", button_click)
+    btn.grid(row=r, column=c+3)
+    c += 1
+    if i%1 == 0:
+        r += 1
+        c=0
 
 btn = tk.Button(root, text=f"=", font=("Times New Roman", 30), width=4, height=2)
 btn.bind("<1>", click_equal)
-btn.grid(row=r, column=c)
+btn.grid(row=r, column=c+3)
+
+entry = tk.Entry(root, justify="right", width=13, font=("Times New Roman", 40))
+entry.grid(row=0, column=0, columnspan=4)
 
 root.mainloop()
