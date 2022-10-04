90938132cac88f78a07a996fd6b577f14945a22b
retsu <c0b21059c5@edu.teu.ac.jp>
2022-10-04 15:22:59 +0900

    練習6まで終わり

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -7,13 +7,15 @@ root.geometry("300x600")
 
 def button_click(event):
     btn = event.widget
-    num = int(btn["text"])
+    num = btn["text"]
     #tkm.showinfo(num, f"{num}のボタンがクリックされました")
     entry.insert(tk.END, num)
 
 r=1
 c=0
-for i, num in enumerate(range(9,-1, -1), 1):
+numbers = list(range(9, -1, -1))
+operators = ["+"]
+for i, num in enumerate(numbers+operators, 1):
     btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width=4, height=2)
     btn.bind("<1>", button_click)
     btn.grid(row=r, column=c)
