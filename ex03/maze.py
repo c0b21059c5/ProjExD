aed9a89873d9daa64bb67edc970edea485211ac6
retsu <c0b21059c5@edu.teu.ac.jp>
2022-10-18 17:16:19 +0900

    追加機能の実装

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 #練習8
 import maze_maker as mm
+import sys
 
 #練習5
 def key_down(event):
@@ -17,26 +18,55 @@ def key_up(event):
 def main_proc():
     global cx, cy
     global mx, my
+    global tori
     if key == "Up":
         my -= 1
+        cy = my*100+50
+        tori = tk.PhotoImage(file="Ex03/fig/6.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
     if key == "Down":
         my += 1
+        cy =  my*100+50
+        tori = tk.PhotoImage(file="Ex03/fig/3.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
     if key == "Right":
         mx += 1
+        cx = mx*100+50
+        tori = tk.PhotoImage(file="Ex03/fig/2.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
     if key == "Left":
         mx -= 1
-    if maze_lst[my][mx] == 0:
+        cx = mx*100+50
+        tori = tk.PhotoImage(file="Ex03/fig/0.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
+    if maze_lst[my][mx] != 1:
         cx, cy = mx*100+50, my*100+50
     else:
         if key == "Up":
             my += 1
+            cy = my*100+50
+            tori = tk.PhotoImage(file="Ex03/fig/3.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
         if key == "Down":
             my -= 1
+            cy =  my*100+50
+            tori = tk.PhotoImage(file="Ex03/fig/3.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
         if key == "Right":
             mx -= 1
+            cx = mx*100+50
+            tori = tk.PhotoImage(file="Ex03/fig/2.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
         if key == "Left":
             mx += 1
+            cx = mx*100+50
+            tori = tk.PhotoImage(file="Ex03/fig/0.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
     canv.coords("tori", cx, cy)
+    if mx == 14 and my == 7:
+        tkm.showinfo("君は天才だ", "おめでとう！！！")
+        sys.exit()
+
     root.after(100, main_proc)
 
 if __name__ == "__main__":
@@ -50,8 +80,8 @@ if __name__ == "__main__":
     canv.pack()
     
     #練習3、11
-    tori = tk.PhotoImage(file="Ex03/fig/3.png")
-    mx, my = 1, 1
+    tori = tk.PhotoImage(file="Ex03/fig/1.png")
+    mx, my = 0, 1
     cx, cy = mx*100, my*100
     
     #練習4
