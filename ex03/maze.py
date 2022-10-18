c7a66e39364af346de3bc34dad4f7ff03cd6abd4
retsu <c0b21059c5@edu.teu.ac.jp>
2022-10-18 15:33:57 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+#練習8
+import maze_maker as mm
 
 #練習5
 def key_down(event):
@@ -11,17 +13,29 @@ def key_up(event):
     global key
     key = ""
 
-#練習7
+#練習7、11,12
 def main_proc():
     global cx, cy
+    global mx, my
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Right":
-        cx += 20
+        mx += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
+    if maze_lst[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Right":
+            mx -= 1
+        if key == "Left":
+            mx += 1
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -35,10 +49,10 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, height=900, width=1500, bg="black")
     canv.pack()
     
-    #練習3
+    #練習3、11
     tori = tk.PhotoImage(file="Ex03/fig/3.png")
-    cx, cy = 300, 400
-    canv.create_image(cx, cy, image=tori, tag="tori")
+    mx, my = 1, 1
+    cx, cy = mx*100, my*100
     
     #練習4
     key=""
@@ -48,8 +62,14 @@ if __name__ == "__main__":
     #練習⑥
     root.bind("<KeyRelease>", key_up)
 
+    #練習9
+    maze_lst = mm.make_maze(15, 9)
+    
     #練習7
     main_proc()
 
+    #練習3,10
+    mm.show_maze(canv, maze_lst)
+    canv.create_image(cx, cy, image=tori, tag="tori")
 
     root.mainloop()
